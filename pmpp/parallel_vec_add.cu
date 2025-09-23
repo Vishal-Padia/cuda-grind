e0e069f906cfae96f2af89370543b45f249145b6
Vishal Padia <vishalpadia9@gmail.com>
2025-09-23 13:30:48 +0000

    minor changes

--- pmpp/parallel_vec_add.cu
+++ pmpp/parallel_vec_add.cu
@@ -7,23 +7,33 @@ __global__ void VecAdd(const float* A, const float* B, float* C, int N) {
 int main() {
     int N = 1048576;
     size_t bytes = N * sizeof(float);
+    
+    // allocate memory
     float *h_A = (float*)malloc(bytes), *h_B = (float*)malloc(bytes), *h_C = (float*)malloc(bytes);
     float *d_A, *d_B, *d_C;
     cudaMalloc(&d_A, bytes); cudaMalloc(&d_B, bytes); cudaMalloc(&d_C, bytes);
+    
     // Initialize h_A, h_B...
     for (int i = 0; i<N; ++i) {
     	h_A[i] = static_cast<float>(i);
-	h_B[i] = static_cast<float>(2 * i);
+		h_B[i] = static_cast<float>(2 * i);
     }
+    
+    // copy data from host to device
     cudaMemcpy(d_A, h_A, bytes, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice);
+    
+    // launch cuda kernel
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
     VecAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+    
+    // copy data from device to host
     cudaMemcpy(h_C, d_C, bytes, cudaMemcpyDeviceToHost);
     for (int i = 0; i < 10; ++i) {
     	std::cout << "C[" << i << "] = " << h_C[i] << "\n";
     }
+    
+    // free up memory
     cudaFree(d_A); cudaFree(d_B); cudaFree(d_C); free(h_A); free(h_B); free(h_C);
 }
-
