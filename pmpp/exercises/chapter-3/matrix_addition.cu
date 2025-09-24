3d46f893e0978c24b8eb97fc4c52f66bb0099641
Vishal Padia <vishalpadia9@gmail.com>
2025-09-24 06:40:37 +0000

    add matrix addition

---
+++ pmpp/exercises/chapter-3/matrix_addition.cu
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+// matrix takes two inputs B and C and produces one output A.
+// Each element of output matrix A is the sum of corresponding elements of the input matrices.
+
+// produces one output matrix element
+__global__ void MatrixAddition(const float* B, const float* C, float* A, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < N && j < N) {
+        int idx = i * N + j;
+        A[idx] = B[idx] + C[idx];
+    }
+}
+
+int main() {
+    int N = 1024;
+    size_t bytes = N * N * sizeof(float);
+
+    // allocate memory
+    float *h_A = (float*)malloc(bytes);
+    float *h_B = (float*)malloc(bytes);
+    float *h_C = (float*)malloc(bytes);
+    float *d_A, *d_B, *d_C;
+    cudaMalloc(&d_A, bytes);
+    cudaMalloc(&d_B, bytes);
+    cudaMalloc(&d_C, bytes);
+
+    // initialize matrices
+    for (int i = 0; i < N * N; ++i) {
+        h_B[i] = static_cast<float>(i);
+        h_C[i] = static_cast<float>(i);
+    }
+
+    // copy data from host to device
+    cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_C, h_C, bytes, cudaMemcpyHostToDevice);
+
+    // launch kernel
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((N + 15) / 16, (N + 15) / 16);
+    MatrixAddition<<<blocksPerGrid, threadsPerBlock>>>(d_B, d_C, d_A, N);
+    
+    // copy data from device to host
+    cudaMemcpy(h_A, d_A, bytes, cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 10; ++i) {
+        std::cout << h_A[i] << "\n";
+    }
+
+    // free up memory
+    cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
+    free(h_A); free(h_B); free(h_C);
+}
